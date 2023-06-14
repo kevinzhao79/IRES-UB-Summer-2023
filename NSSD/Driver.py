7a48b9050657c4e7fba02ab75fa0b85780d3d8c8
zhaok <kczhao@wisc.edu>
2023-06-14 12:22:20 +0100

    Cleaned up code in Driver and Pre classes (6/14)

--- NSSD/Driver.py
+++ NSSD/Driver.py
@@ -1,4 +1,4 @@
-import os, sys
+import sys
 
 import pocketsphinx as ps
 from pocketsphinx import AudioFile
@@ -6,72 +6,119 @@ from pocketsphinx import AudioFile
 import Setup as Setup
 from Setup import Setup
 import Word as Word
-from Word import Word
 import PreNSSDProcess as PreNSSDProcess
 from PreNSSDProcess import PreNSSDProcess
 
 """
-The driver class for the baseline NSSD program. The CLA for this program are as follows:
+The driver class for the baseline NSSD program. 
+The Command Line Arguments for this program are as follows:
 
-python3 Driver.py [relative path of audio file] [keyword 1] [keyword 2] ... [keyword <n>] [keyword_threshold]
+python3 Driver.py [relative path of audio file] [keyword 1] [keyword 2] ... [keyword n] [keyword_threshold]
 """
-def main():
 
-    if len(sys.argv) < 4:
-        print("Error: Incorrect number of Command-Line Arguments: Should be at least 4, but was " + str(len(sys.argv)))
-        return
+class Driver:
+
+    setup = None
+    pre = None
+    file = None
+    nss = []
+    threshold = None
+
+
+    def __init__(self, setup, pre):
+        self.setup = setup
+        self.pre = pre
+
+    def check_argv(self):
+        if len(sys.argv) < 4:
+            print("Error: Incorrect number of Command-Line Arguments: Should be at least 4, but was " + str(len(sys.argv)))
+            return
 
     #convert CMA into local vars
-    file = sys.argv[1]
-    nss = []
-    for i in range(2, len(sys.argv) - 1):
-        nss.append(sys.argv[i])
-    threshold = sys.argv[len(sys.argv) - 1]
-
+    def update_argv(self):
+        self.file = sys.argv[1]
+        for i in range(2, len(sys.argv) - 1):
+            self.nss.append(sys.argv[i])
+        self.threshold = sys.argv[len(sys.argv) - 1]
 
     #see if the file path is valid
-    try:
-        audio = AudioFile(file)
-        Setup.transcribe(audio)
-        Setup.separate(Setup.ts_transcription)
-    except:
-        print("Error: audio file path invalid.\n")
+    def check_audio(self):
+        try:
+            audio = AudioFile(self.file)
+            self.setup.transcribe(audio)
+            self.setup.separate(self.setup.ts_transcription)
+        except:
+            print("Error: audio file path invalid.\n")
+            return
 
     #see if the keywords are parseable as a str
-    for i in range(0, nss.__len__()):
-        if not isinstance(nss[i], str):
-            print("Error: keyword " + str(i) + " is not of type string.\n")
-            return
+    def check_kw(self):
+        for i in range(0, self.nss.__len__()):
+            if not isinstance(self.nss[i], str):
+                print("Error: keyword " + str(i) + " is not of type string.\n")
+                return
 
     #see if the keyword threshold is a number and between 1e-1 and 1e-50
-    try:
-        num_threshold = float(threshold)
-        if num_threshold > 1e-1 or num_threshold < 1e-50:
-            print("Error: keyword threshold must be between 1e-1 and 1e-50.")
-            print(threshold)
+    def check_threshold(self):
+        try:
+            self.threshold = float(self.threshold)
+            if self.threshold > 1e-1 or self.threshold < 1e-50:
+                print("Error: keyword threshold must be between 1e-1 and 1e-50.")
+                return
+        except:
+            print("Error: keyword threshold not parseable as an int.")
             return
-    except:
-        print("Error: keyword threshold not parseable as an int.")
-        print(type(num_threshold))
-        return
 
-    process = PreNSSDProcess(Setup.word_list)
-    process.detect_pause(Setup.word_list)
-    for i in range(0, nss.__len__()):
-        process.detect_nss(file, nss[i], num_threshold)
+    #Initializes the PreNSSDProcess object needed to run the program
+    def init_pre(self):
+        self.pre = PreNSSDProcess(self.setup.word_list)
+        self.pre.detect_pause(self.setup.word_list)
+
+        #Run separate NSS detection tests for each NSS
+        for i in range(0, self.nss.__len__()):
+            self.pre.detect_nss(self.file, self.nss[i], self.threshold)
+
+        self.pre.combine()
+        self.pre.remove_dups()
+        self.pre.sort_lists()
+        self.pre.update_size()
+
+    def output(self):
+        print("Number of NSS sounds detected: " + str(self.pre.num_nss))
+        print("Number of pauses detected: " + str(self.pre.num_pause))
+
+        #prints just the words of the combined transcription
+        string = "\n"
+        for word in self.pre.combined_transcription:
+            string += word.name
+            string += " "
+        string += "\n"
+        print(string)
+
+    #Handler function that calls all other driver functions
+    def driver_handler(self):
+        self.check_argv()
+        self.update_argv()
+        self.check_audio()
+        self.check_kw()
+        self.check_threshold()
+        self.init_pre()
+        self.output()
+
+def main():
+
+    driver = Driver(Setup, PreNSSDProcess)
+    driver.driver_handler()
+
+
     
-    process.sort_nss()
-    process.combination_transcription()
 
-    print("Number of NSS sounds detected: " + str(process.num_nss))
-    print("Number of pauses detected: " + str(process.num_pause))
+    
+    
+    
+    
 
-    string = "\n"
-    for word in process.combined_transcription:
-        string += word.name
-        string += " "
-    string += "\n"
-    print(string)
+    
 
      
 main()
--- NSSD/PreNSSDProcess.py
+++ NSSD/PreNSSDProcess.py
@@ -1,13 +1,11 @@
 import pocketsphinx as ps
 from pocketsphinx import AudioFile
 import Setup as Setup
-from Setup import Setup
-import collections
 import Word as Word
 from Word import Word
 
 
-"""Completes requirements for pre-NSSD processing, including detecting basic NSS ('uh') and pauses (Setup)"""
+"""Completes requirements for pre-NSSD processing, including detecting basic NSS's and pauses"""
 class PreNSSDProcess:
     
      #number of detectable pauses within the original transcription
@@ -17,39 +15,32 @@ class PreNSSDProcess:
      num_nss = 0
 
      #all potential NSS's in segment form
-     nss_transcription = None
+     nss_transcription = []
 
-     #all potential NSS's in list form
-     potential_nss = []
-
-     #all NSS's that were picked up after the first filter
-     filtered_nss = []
-
-     #all NSS's that were picked up after the second filter
-     double_filtered_nss = []
+     #all NSS's in list form
+     nss_list = []
 
      #list of words obtained from setup containing full transcription
-     word_lst = []
+     word_list = []
 
      #combination of double_filtered_nss and word_lst
      combined_transcription = []
 
-     def __init__(self, word_lst):
-         self.word_lst = word_lst
+     def __init__(self, word_list):
+         self.word_list = word_list
 
      def wordify(self):
           for unit in self.nss_transcription:
             new_word = Word(unit[0], unit[2], unit[3], unit[1])
-            self.potential_nss.append(new_word)
+            self.nss_list.append(new_word)
 
      """determines total num of pauses and increments numpause"""
-     def detect_pause(self, word_lst):
-        for unit in word_lst:
-          #if PS marks unit as silence, increment numpause 
+     def detect_pause(self, word_list):
+        for unit in word_list:
           if unit.name == "<sil>":
                self.num_pause += 1
                unit.name = "<pause>"
-               self.filtered_nss.append(unit)
+               self.nss_list.append(unit)
 
      #finds all potential instances of NSS in transcription, including sounds within words
      def find_all(self, file, nss, threshold):
@@ -65,68 +56,68 @@ class PreNSSDProcess:
      def nss_filter(self): 
 
           #first filter of three
-          nss_filtered_once = filter(self.nss_filter_helper, self.potential_nss)
+          nss_filtered_once = filter(self.nss_filter_helper, self.nss_list)
           for nss in nss_filtered_once:
-               self.filtered_nss.append(nss)
+               self.nss_list.remove(nss)
 
           #second filter of three
-          nss_filtered_twice = filter(self.self_filter, self.filtered_nss)
+          nss_filtered_twice = filter(self.self_filter, self.nss_list)
           for nss in nss_filtered_twice:
-               self.double_filtered_nss.append(nss)
+               self.nss_list.remove(nss)
      
      #1. Grab start/end times of NSS to look through (repeat for all NSS in list)
      #2. Loop through transcription list to check if any words fall into that range
      #3. If so, then the NSS is invalid, and vice versa
      #4. Output all valid NSS into the filtered list
-     def nss_filter_helper(self, potential_nss):
-          start = potential_nss.start
-          end = potential_nss.end
+     def nss_filter_helper(self, nss):
+          start = nss.start
+          end = nss.end
 
-          for word in self.word_lst:
+          for word in self.word_list:
                if word.start - 3 <= start and word.end - 3 >= end: 
-                    return False
+                    return True
 
-          return True
+          return False
          
      #calls find_all and filter to obtain final nss count
      def detect_nss(self, file, nss, threshold):
          self.find_all(file, nss, threshold)
          self.nss_filter()
 
-     #sorts filtered_nss list by start time, ascending
-     def sort_nss(self):
-          #removes duplicates from the list by converting to and back from a dictionary
-          self.double_filtered_nss = list(dict.fromkeys(self.double_filtered_nss))
-
-          #sorts the filtered list based on start times
-          self.double_filtered_nss.sort(key=lambda w: w.start)
-
-          self.num_nss = self.double_filtered_nss.__len__() - self.num_pause
-
-     #filters through all NSS's in the initial filtered list, and removes any 'duplicate' NSS's
-     def self_filter(self, filtered_nss):
-          start = filtered_nss.start
-          end = filtered_nss.end
-          score = filtered_nss.score
-
-          #threshold: .03 seconds
-          for word in self.filtered_nss:
-               if (abs(word.start - start) <= 3 or abs(word.end - end) <= 3) and score < word.score:
-                    return False
-          
-          return True
-
-     #combines doubly filtered NSS list and word transcription list
-     def combination_transcription(self):
-          for word in self.word_lst:
-               self.combined_transcription.append(word)
-          
-          for nss in self.double_filtered_nss:
-               self.combined_transcription.append(nss)
-          
+     #removes duplicates from the nss and combined lists by converting to and back from a dictionary
+     def remove_dups(self):
+          self.nss_list = list(dict.fromkeys(self.nss_list))
           self.combined_transcription = list(dict.fromkeys(self.combined_transcription))
+
+     #sorts nss and combined lists by start time, ascending
+     def sort_lists(self):
+          self.nss_list.sort(key=lambda w: w.start)
           self.combined_transcription.sort(key=lambda w: w.start)
 
+     #Updates the number of NSS's in the combined transcription
+     def update_size(self):
+          self.num_nss = self.nss_list.__len__() - self.num_pause
+
+     #filters through all NSS's in the initial filtered list, and removes any 'duplicate' NSS's
+     def self_filter(self, nss):
+          start = nss.start
+          end = nss.end
+          score = nss.score
+
+          #current threshold: .03 seconds
+          for word in self.nss_list:
+               if (abs(word.start - start) <= 3 or abs(word.end - end) <= 3) and score < word.score:
+                    return True
+          
+          return False
+
+     #combines doubly filtered NSS list and word transcription list
+     def combine(self):
+          for word in self.word_list:
+               self.combined_transcription.append(word)
+          
+          for nss in self.nss_list:
+               self.combined_transcription.append(nss)
                
      #combines all pre-NSSD processed information into the desired output format
      def __repr__(self):
